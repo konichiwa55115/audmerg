2fdb32569a201b3ce5ce7c4a2bce0f3bade6d359
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-01 20:10:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = Client(
     "audiobot",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="5714654934:AAEVIR8baWhJcgUOtWeNmrSjvdRfYRiY7tI"
+    bot_token="6164763193:AAGwXkwq71D36sV-iMH39m7qEJ0mfGvKDM0"
 )
 @bot.on_message(filters.private & filters.incoming & filters.text )
 def _telegram_file(client, message):
